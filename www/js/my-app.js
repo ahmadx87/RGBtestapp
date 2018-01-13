085591a4c77d04a667fbaad135f3aa09db622d92
ahmadx87 <ahmadx87@yahoo.com>
2018-01-13 18:38:43 +0330

    Update my-app.js

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,17 +1,17 @@
 // Dom7
 var $$ = Dom7;
+animations = ["رنگین کمان", "رنگین کمان+درخشش", "رنگین کمان+درخشش 2", "تپش تصادفی", "ستاره دنباله دار", "3 ستاره دنباله دار", "رفت و برگشتی", "7 رنگ پیوسته"];
 
-// Theme
-var theme = 'auto';
-if (document.location.search.indexOf('theme=') >= 0) {
-  theme = document.location.search.split('theme=')[1].split('&')[0];
-}
-
+var rangeObj=[];
 // Init App
 var myApp = new Framework7({
   id: 'io.framework7.testapp',
   root: '#app',
-  theme: theme,
+  theme: 'md',
+    smartSelect: {
+    pageTitle: 'Select Option',
+    closeOnSelect: true,
+  },
   data: function () {
     return {
       user: {
@@ -22,13 +22,10 @@ var myApp = new Framework7({
   },
   methods: {
     helloWorld: function () {
-      app.dialog.alert('Hello World!');
+      myApp.dialog.alert('Hello World!');
     },
   },
-  routes: routes,
-  vi: {
-    placementId: 'pltd4o7ibb9rc653x14',
-  },
+  //routes: routes,
 });
 
 
@@ -43,6 +40,7 @@ $$(document).on('deviceready', function() {
 
 // Option 2. Using one 'pageInit' event handler for all pages:
 $$(document).on('pageInit', function (e) {	
+
     // Get page data from event data
     var page = e.detail.page;
 	console.log('1');
@@ -50,15 +48,26 @@ $$(document).on('pageInit', function (e) {
         // Following code will be executed for page with data-page attribute equal to "about"
         myApp.alert('Here comes About page');
     }
+	
 })
 
 
-$$(document).on('deviceready', function () { console.log('1'); });
-
 document.addEventListener("DOMContentLoaded", function(event) { 
-  console.log('1');
-  
-  	/// Brightness slider
+	var selectionHTMLstring='';
+	for ( var i = 0; i < animations.length; i++) {
+		selectionHTMLstring += '<option data-option-class="koodak-font" value="' + i + '">' + animations[i] + '</option>';
+	}
+	document.getElementById('animationListSelectElement').innerHTML=selectionHTMLstring; 
+ 
+ 	var listHTMLstring='';
+	for ( var i = 0; i < animations.length; i++) {
+		listHTMLstring += $listAddItem(animations[i]);
+	}
+	//listHTMLstring= '<ul>' + listHTMLstring + '</ul>';
+	//$('#animationListDiv').html(listHTMLstring);
+	document.querySelector('#animationListDiv ul').innerHTML=listHTMLstring;
+	
+	/// Brightness slider
 
 	/*$('#brightnessSlider').slider({
 		stop: function (event, ui) {
@@ -68,7 +77,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		}
 	});*/
 
-	var lastreq = 0;
+var lastreq=0;
 	$('#brightnessSlider').on("change", function () {
 		localStorage.setItem("brightnessValue", $('#brightnessSlider').val());
 		var d = new Date();
@@ -83,7 +92,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 	});
   
-  setTimeout(function () {
+
 		widthSize = $(window).width() * .65;
 		$('#colorPickerDiv').css({
 			'width': widthSize,
@@ -98,12 +107,89 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		});
 
 
-	}, 0);
+  myApp.on('sortableSort', function (listEl, indexes) {
+  console.log(indexes);
+});
+
+myApp.sortable.enable('.sortable');  
+
+$$('.open-vertical').on('click', function () {
+//document.getElementById('greenSlider').value=200;
+myApp.dialog.create({
+  title: 'تنظیم رنگ',
+  verticalButtons: false,
+  cssClass: 'appdialog',
+  buttons:[{text:'تایید'},{text:'انصراف'}],
+  on:{
+    open:function(d){
+      var els=d.$el.find('.range-slider');
+	  console.log(els);
+		var color = $('#colorPickerInput').wheelColorPicker('getColor');
+		colorR = Math.round(color.r * 255);
+		colorG = Math.round(color.g * 255);
+		colorB = Math.round(color.b * 255);
+		colorarr=[colorR,colorG,colorB];
+	  
+      for(var i=0;i<els.length;i++){
+		  els[i].value=200;
+        rangeObj[i] = myApp.range.create({
+          el: els[i],
+          on:{
+            change:function(r){
+              //console.log(r.getValue());
+            }
+          }
+        });
+		rangeObj[i].setValue(colorarr[i]);
+		console.log(rangeObj[i]);
+		//myApp.range.get('.range-slider');
+      }
+		//console.log(myApp.range.get('#blueSlider').getValue());
+		//console.log($('#blueSlider').val);
+		myApp.range.setValue(document.getElementsByClassName('color-green'),200);
+		//document.getElementById('greenSlider').value=200;
+		console.log(document.getElementById('greenSlider').value);
+
+      
+    }
+  },
+  content: document.getElementById('rgbSliderPickerPopUp').innerHTML,
+}).open();
+
+
+});
   
-  
-  
-  
-  
+
+
+$('#animationListDiv').on('click','.listAddItem',function(){
+	myApp.smartSelect.open('#animationListSelect');
+	$(this).parent().parent().after($listAddItem('جدید'+Math.round(Math.random()*100)));
+});
+
+$$('#redSlider').on('range:change', function (e, range) {
+  console.log(range.value);
+});
+
+$('.hjhjhjhj').on('click',function(){
+	console.log('sss');
+});
   
 });
 
+
+//This function returns the html for new item to add
+function $listAddItem(item){
+return '<li class="swipeout">\
+          <div class="item-content swipeout-content">\
+            <div class="item-inner">\
+              <div class="item-title">'+item+' </div>\
+            </div>\
+          </div>\
+          <div class="sortable-handler"></div>\
+		        <div class="swipeout-actions-right">\
+        <a href="#" class="color-green listAddItem"><i class="material-icons">add</i></a>\
+		<a href="#" class="color-blue listEditItem"><i class="material-icons">edit</i></a>\
+        <a href="#" class="swipeout-delete"><i class="material-icons">delete</i></a>\
+      </div>\
+        </li>'
+}
